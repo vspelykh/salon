4dfb28a8c062b5e2a2fee6ea39155b754ae222e7
Vladyslav Pelykh <vspelykh@gmail.com>
2023-07-23 19:08:01 +0300

    Normalize DB: Fix multilanguage issue, fix user_level relation to users table
    Signed-off-by: Vladyslav Pelykh <vspelykh@gmail.com>

--- src/main/java/ua/vspelykh/salon/listener/ScheduledEmailExecutorListener.java
+++ src/main/java/ua/vspelykh/salon/listener/ScheduledEmailExecutorListener.java
@@ -1,28 +1,6 @@
 package ua.vspelykh.salon.listener;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ua.vspelykh.salon.model.dto.AppointmentDto;
-import ua.vspelykh.salon.model.entity.AppointmentStatus;
-import ua.vspelykh.salon.service.ServiceFactory;
-import ua.vspelykh.salon.service.email.EmailService;
-import ua.vspelykh.salon.service.impl.ServiceFactoryImpl;
-import ua.vspelykh.salon.util.exception.ServiceException;
-
-import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.text.MessageFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import static ua.vspelykh.salon.listener.ListenerConstants.*;
 
 /**
  * This class is a ServletContextListener that schedules a task to send emails every morning to all visitors who
@@ -32,81 +10,81 @@ import static ua.vspelykh.salon.listener.ListenerConstants.*;
  */
 public class ScheduledEmailExecutorListener implements ServletContextListener {
 
-    private static final Logger LOG = LogManager.getLogger(ScheduledEmailExecutorListener.class);
-
-    /**
-     * This method is called by the servlet container when the context is initialized. It schedules the email task to run
-     * every day at 9:00 AM.
-     *
-     * @param sce The ServletContextEvent object.
-     */
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        executeTask();
-    }
-
-    /**
-     * This method is called by the servlet container when the context is destroyed.
-     *
-     * @param sce The ServletContextEvent object.
-     */
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        ServletContextListener.super.contextDestroyed(sce);
-    }
-
-    /**
-     * This method schedules the email task to run every day at 9:00 AM.
-     */
-    private void executeTask() {
-
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(KYIV));
-        ZonedDateTime nextRun = now.withHour(9).withMinute(0).withSecond(0);
-        if (now.compareTo(nextRun) > 0)
-            nextRun = nextRun.plusDays(1);
-
-        Duration duration = Duration.between(now, nextRun);
-        long initialDelay = duration.getSeconds();
-
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate((getTask()),
-                initialDelay,
-                TimeUnit.DAYS.toSeconds(1),
-                TimeUnit.SECONDS);
-    }
-
-    /**
-     * This method retrieves all appointments from the previous day and sends feedback emails to visitors who had
-     * appointment with a status of SUCCESS.
-     *
-     * @return A Runnable object that represents the email task.
-     */
-    private Runnable getTask() {
-        try {
-            ServiceFactory serviceFactory = ServiceFactoryImpl.getServiceFactory();
-            List<AppointmentDto> appointments = serviceFactory.getAppointmentService().getAllByDate(LocalDate.now().minusDays(1));
-            if (!appointments.isEmpty()) {
-                return () -> {
-                    for (AppointmentDto appointment : appointments) {
-                        if (appointment.getStatus() == AppointmentStatus.SUCCESS) {
-                            String clientName = appointment.getClient().getName() + " " + appointment.getClient().getSurname();
-                            String masterName = appointment.getMaster().getName() + " " + appointment.getMaster().getSurname();
-                            String feedbackLink = "http://localhost:8080/salon?command=feedback&id=" + appointment.getId();
-                            String email = String.format(FEEDBACK_EMAIL, clientName, masterName, feedbackLink);
-                            EmailService.sendEmail(appointment.getClient().getEmail(), FEEDBACK_THEME, email);
-                            if (LOG.isEnabled(Level.INFO)) {
-                                LOG.info(MessageFormat.format("Email for appointment id={0} sent.", appointment.getId()));
-                            }
-                        }
-                    }
-                };
-            }
-        } catch (ServiceException e) {
-            LOG.error("Error to get appointments for emails");
-        }
-        LOG.info("No appointments for send emails");
-        return () -> {
-
-        };
-    }
+//    private static final Logger LOG = LogManager.getLogger(ScheduledEmailExecutorListener.class);
+//
+//    /**
+//     * This method is called by the servlet container when the context is initialized. It schedules the email task to run
+//     * every day at 9:00 AM.
+//     *
+//     * @param sce The ServletContextEvent object.
+//     */
+//    @Override
+//    public void contextInitialized(ServletContextEvent sce) {
+//        executeTask();
+//    }
+//
+//    /**
+//     * This method is called by the servlet container when the context is destroyed.
+//     *
+//     * @param sce The ServletContextEvent object.
+//     */
+//    @Override
+//    public void contextDestroyed(ServletContextEvent sce) {
+//        ServletContextListener.super.contextDestroyed(sce);
+//    }
+//
+//    /**
+//     * This method schedules the email task to run every day at 9:00 AM.
+//     */
+//    private void executeTask() {
+//
+//        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(KYIV));
+//        ZonedDateTime nextRun = now.withHour(9).withMinute(0).withSecond(0);
+//        if (now.compareTo(nextRun) > 0)
+//            nextRun = nextRun.plusDays(1);
+//
+//        Duration duration = Duration.between(now, nextRun);
+//        long initialDelay = duration.getSeconds();
+//
+//        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+//        scheduler.scheduleAtFixedRate((getTask()),
+//                initialDelay,
+//                TimeUnit.DAYS.toSeconds(1),
+//                TimeUnit.SECONDS);
+//    }
+//
+//    /**
+//     * This method retrieves all appointments from the previous day and sends feedback emails to visitors who had
+//     * appointment with a status of SUCCESS.
+//     *
+//     * @return A Runnable object that represents the email task.
+//     */
+//    private Runnable getTask() {
+//        try {
+//            ServiceFactory serviceFactory = ServiceFactoryImpl.getServiceFactory();
+//            List<AppointmentDto> appointments = serviceFactory.getAppointmentService().getAllByDate(LocalDate.now().minusDays(1));
+//            if (!appointments.isEmpty()) {
+//                return () -> {
+//                    for (AppointmentDto appointment : appointments) {
+//                        if (appointment.getStatus() == AppointmentStatus.SUCCESS) {
+//                            String clientName = appointment.getClient().getName() + " " + appointment.getClient().getSurname();
+//                            String masterName = appointment.getMaster().getName() + " " + appointment.getMaster().getSurname();
+//                            String feedbackLink = "http://localhost:8080/salon?command=feedback&id=" + appointment.getId();
+//                            String email = String.format(FEEDBACK_EMAIL, clientName, masterName, feedbackLink);
+//                            EmailService.sendEmail(appointment.getClient().getEmail(), FEEDBACK_THEME, email);
+//                            if (LOG.isEnabled(Level.INFO)) {
+//                                LOG.info(MessageFormat.format("Email for appointment id={0} sent.", appointment.getId()));
+//                            }
+//                        }
+//                    }
+//                };
+//            }
+//        } catch (ServiceException e) {
+//            LOG.error("Error to get appointments for emails");
+//        }
+//        LOG.info("No appointments for send emails");
+//        return () -> {
+//
+//        };
+//    }
 }
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,12 +1,16 @@
-DROP TABLE IF EXISTS user_level;
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS feedbacks;
 DROP TABLE IF EXISTS appointment_items;
 DROP TABLE IF EXISTS master_services;
+DROP TABLE IF EXISTS base_service_translation;
 DROP TABLE IF EXISTS base_services;
+DROP TABLE IF EXISTS service_category_translation;
 DROP TABLE IF EXISTS service_categories;
 DROP TABLE IF EXISTS appointments;
 DROP TABLE IF EXISTS working_days;
+DROP TABLE IF EXISTS user_level_translation;
+DROP TABLE IF EXISTS user_level;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS consultations;
 DROP TABLE IF EXISTS invitations;
@@ -22,110 +26,132 @@ CREATE TABLE users
     password VARCHAR(500)       NOT NULL
 );
 
-CREATE UNIQUE INDEX users_unique_email_idx ON users (number);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE INDEX idx_users_name_surname ON users (name, surname);
 
-CREATE TABLE user_level
+CREATE TABLE roles
 (
-    id       INTEGER NOT NULL,
-    level    VARCHAR NOT NULL,
-    about    VARCHAR NOT NULL,
-    about_ua VARCHAR NOT NULL,
-    active   BOOLEAN NOT NULL,
-    CONSTRAINT user_level_idx UNIQUE (id),
-    FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
+    id   SERIAL PRIMARY KEY,
+    role VARCHAR NOT NULL
 );
 
 CREATE TABLE user_roles
 (
-    user_id INTEGER NOT NULL,
-    role    VARCHAR NOT NULL,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    role_id INTEGER NOT NULL REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_level
+(
+    id     INTEGER PRIMARY KEY,
+    level  VARCHAR NOT NULL,
+    about  VARCHAR NOT NULL,
+    active BOOLEAN NOT NULL
+);
+
+CREATE TABLE user_level_translation
+(
+    id            SERIAL PRIMARY KEY,
+    user_level_id INTEGER    NOT NULL REFERENCES user_level (id) ON DELETE CASCADE,
+    language_code VARCHAR(2) NOT NULL,
+    about         VARCHAR    NOT NULL
 );
 
 CREATE TABLE service_categories
 (
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR NOT NULL,
-    name_ua VARCHAR NOT NULL
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE service_category_translation
+(
+    service_category_id INTEGER    NOT NULL REFERENCES service_categories (id) ON DELETE CASCADE,
+    language_code       VARCHAR(2) NOT NULL,
+    translated_name     VARCHAR    NOT NULL,
+    PRIMARY KEY (service_category_id, language_code)
 );
 
 CREATE TABLE base_services
 (
     id          SERIAL PRIMARY KEY,
-    category_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL REFERENCES service_categories (id),
     service     VARCHAR NOT NULL,
-    service_ua  VARCHAR NOT NULL,
-    price       INTEGER NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES service_categories (id)
+    price       INTEGER NOT NULL
+);
+
+CREATE TABLE base_service_translation
+(
+    id                 SERIAL PRIMARY KEY,
+    base_service_id    INTEGER    NOT NULL REFERENCES base_services (id) ON DELETE CASCADE,
+    language_code      VARCHAR(2) NOT NULL,
+    translated_service VARCHAR    NOT NULL
 );
 
 CREATE TABLE master_services
 (
     id              SERIAL PRIMARY KEY,
-    master_id       INTEGER NOT NULL,
-    base_service_id INTEGER NOT NULL,
-    continuance     INTEGER NOT NULL,
-    FOREIGN KEY (master_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (base_service_id) REFERENCES base_services (id)
+    master_id       INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    base_service_id INTEGER NOT NULL REFERENCES base_services (id),
+    continuance     INTEGER NOT NULL
 );
 
 CREATE TABLE appointments
 (
     id             SERIAL PRIMARY KEY,
-    master_id      INTEGER             NOT NULL,
-    client_id      INTEGER             NOT NULL,
-    continuance    INTEGER             NOT NULL,
-    date           TIMESTAMP           NOT NULL,
-    price          INTEGER             NOT NULL,
-    discount       INTEGER DEFAULT '1' NOT NULL,
-    status         VARCHAR             NOT NULL,
-    payment_status VARCHAR             NOT NULL,
-    FOREIGN KEY (master_id) REFERENCES users (id),
-    FOREIGN KEY (client_id) REFERENCES users (id)
+    master_id      INTEGER           NOT NULL REFERENCES users (id),
+    client_id      INTEGER           NOT NULL REFERENCES users (id),
+    continuance    INTEGER           NOT NULL,
+    date           TIMESTAMP         NOT NULL,
+    price          INTEGER           NOT NULL,
+    discount       INTEGER DEFAULT 1 NOT NULL,
+    status         VARCHAR           NOT NULL,
+    payment_status VARCHAR           NOT NULL
 );
 
 CREATE TABLE appointment_items
 (
-    id             SERIAL PRIMARY KEY,
-    appointment_id INTEGER NOT NULL,
-    service_id     INTEGER NOT NULL,
-    FOREIGN KEY (service_id) REFERENCES master_services (id),
-    FOREIGN KEY (appointment_id) REFERENCES appointments (id) ON DELETE CASCADE
+    id                SERIAL PRIMARY KEY,
+    appointment_id    INTEGER NOT NULL REFERENCES appointments (id) ON DELETE CASCADE,
+    master_service_id INTEGER NOT NULL REFERENCES master_services (id)
 );
-
 
 CREATE TABLE feedbacks
 (
     id             SERIAL PRIMARY KEY,
-    appointment_id INTEGER   NOT NULL,
+    appointment_id INTEGER   NOT NULL REFERENCES appointments (id) ON DELETE CASCADE,
     mark           INTEGER   NOT NULL,
     comment        VARCHAR   NOT NULL,
-    date           TIMESTAMP NOT NULL,
-    FOREIGN KEY (appointment_id) REFERENCES appointments (id) ON DELETE CASCADE
+    date           TIMESTAMP NOT NULL
 );
 
 CREATE UNIQUE INDEX mark_unique_appointment_idx ON feedbacks (appointment_id);
 
 CREATE TABLE consultations
 (
-    id     SERIAL PRIMARY KEY,
-    name   VARCHAR NOT NULL,
-    number VARCHAR NOT NULL,
-    date   TIMESTAMP DEFAULT now(),
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR NOT NULL,
+    number  VARCHAR NOT NULL,
+    date    TIMESTAMP DEFAULT now(),
     is_read BOOLEAN   DEFAULT FALSE
 );
 
 CREATE TABLE working_days
 (
     id         SERIAL PRIMARY KEY,
-    user_id    INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     date       DATE    NOT NULL,
     time_start TIME    NOT NULL,
-    time_end   TIME    NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT user_working_day UNIQUE (user_id, date)
+    time_end   TIME    NOT NULL
 );
+
+CREATE INDEX idx_master_services_master_id ON master_services (master_id);
+CREATE INDEX idx_working_days_user_id ON working_days (user_id);
+CREATE INDEX idx_appointments_master_id ON appointments (master_id);
+CREATE INDEX idx_feedbacks_appointment_id ON feedbacks (appointment_id);
+CREATE INDEX idx_appointment_items_appointment_id ON appointment_items (appointment_id);
 
 CREATE TABLE invitations
 (
